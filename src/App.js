614b3ec715a54fadf76d668e722a4d305d9d0b38
Tyler Blackburn <jtblackburn13@yahoo.com>
2020-08-20 23:33:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-logo">~~~~~VENTURE_NAME~~~~~</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ~~~~~TAG_LINE~~~~~
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
